5b807a0ba5a04b954c9b923cb23102c1abdf0c27
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2021-06-15 13:57:53 +0900

    SQL接続 1つにした

--- PH22_term2/IH12B/30/list.php
+++ PH22_term2/IH12B/30/list.php
@@ -5,21 +5,12 @@ require_once 'config/err_code.php';
 require_once 'model/controller.php';
 require_once 'model/initialize.php';
 
-echo '$_POST:';
-var_dump($_POST);
-echo '<br>';
-
-// 検索ボタン押してない
-if (!isset($_POST['sch_name'])) {
-    // 全件表示
-    $link = sql_connect('../../const.php');
-    $sql = "SELECT * FROM sample";
-    $result = sql_query($link, $sql, 'select');
-}
-// 検索ボタン押した
+//
+$link = sql_connect('../../const.php');
+// 受け取り処理
 if (isset($_POST['sch_name'])) {
     $sch_name = $_POST['sch_name'];
-    // 検索名を含む名前のあるレコードを表示
+
     if ($sch_name === '') {
         $result = [];
         $_SESSION['msg'] = '対象がありません';
@@ -31,6 +22,9 @@ if (isset($_POST['sch_name'])) {
         WHERE name like '%" . $sch_name . "%'";
         $result = sql_query($link, $sql, 'select');
     }
+} else {
+    $sql = "SELECT * FROM sample";
+    $result = sql_query($link, $sql, 'select');
 }
 // msg表示
 if (isset($_SESSION['msg'])) {
--- PH22_term2/IH12B/30/tpl/list.php
+++ PH22_term2/IH12B/30/tpl/list.php
@@ -14,69 +14,57 @@
     </div>
     <div class="row g-3 ">
         <form method="post" class="row g-3 shadow justify-content-sm-center mb-5">
-                <div class="col-auto">
-                    <a href="list.php"><img src="img/people_logo2.png" class="img-fluid " alt="ガラス玉,google風" style="height:50px; max-width:100%;"></a>
-                </div>
+            <div class="col-auto">
+                <a href="list.php"><img src="img/people_logo2.png" class="img-fluid " alt="ガラス玉,google風" style="height:50px; max-width:100%;"></a>
+            </div>
             <!-- <div class="col-auto">
                         <label for="SEARCH">name:</label>
                     </div> -->
-                <div class="col-sm-6 input-group mb-3 col-auto w-50">
-                    <input type="search" name="sch_name" class="form-control" placeholder="Peopleで検索または人名を入力" aria-label="Recipient's username" aria-describedby="button-addon2" id="SEARCH" autofocus>
-                    <button type="submit" name="sch_name" class="btn btn-secondary"  id="button-addon2">search</button>
+            <div class="col-sm-6 input-group mb-5 col-auto w-50">
+                <input type="search" name="sch_name" class="form-control" placeholder="Peopleで検索または人名を入力" aria-label="Recipient's username" aria-describedby="button-addon2" autofocus>
+                <!-- id="SEARCH" -->
+                <button type="submit" value="push" class="btn btn-secondary" id="button-addon2">search</button>
+            </div>
 
-                </div>
-
-                <div class="row g-3 mb-4 justify-content-sm-center">
-                    <!-- <div class="col-auto">
-                        <input type="checkbox" class="btn-check" id="btn-check" autocomplete="off">
-                        <label class="btn shadow" for="btn-check">ID</label>
-                    </div>
-                    <div class="col-auto">
-                        <input type="checkbox" class="btn-check" id="btn-check" autocomplete="off">
-                        <label class="btn shadow" for="btn-check">NAME</label>
-                    </div>
-                    <div class="col-auto">
-                        <input type="checkbox" class="btn-check" id="btn-check-2" checked autocomplete="off">
-                        <label class="btn shadow" for="btn-check-2">AGE</label>
-                    </div> -->
-                    <select name="order_c"class="col-auto form-select w-25" aria-label="Default select example">
-                        <option value="id" selected>ID</option>
-                        <option value="name">NAME</option>
-                        <option value="age">AGE</option>
-                    </select>
-                    <select name="order" class="col-auto form-select w-25" aria-label="Default select example">
-                        <option value="ASC" selected>昇順</option>
-                        <option value="DESC">降順</option>
-                    </select>
-                </div>
+            <!-- <div class="row g-3 mb-4 justify-content-sm-center">
+                <select name="order_c" class="col-auto form-select w-25" aria-label="Default select example">
+                    <option value="id" selected>ID</option>
+                    <option value="name">NAME</option>
+                    <option value="age">AGE</option>
+                </select>
+                <select name="order" class="col-auto form-select w-25" aria-label="Default select example">
+                    <option value="ASC" selected>昇順</option>
+                    <option value="DESC">降順</option>
+                </select>
+            </div> -->
 
         </form>
 
     </div>
 
     <div class="container-sm justify-content-sm-center">
-            <div class="alert alert-secondary lead" role="alert"><?php echo $msg; ?></div>
-            <table class="table table-striped">
-                <!-- class="table table-dark table-striped" -->
+        <div class="alert alert-secondary lead" role="alert"><?php echo $msg; ?></div>
+        <table class="table table-striped">
+            <!-- class="table table-dark table-striped" -->
+            <tr>
+                <th scope="col"><?php echo '約' . count($result) . '件' ?></th>
+            </tr>
+            <tr>
+                <th>ID</th>
+                <th>名前</th>
+                <th>年齢</th>
+            </tr>
+            <?php foreach ($result as $key => $value) : ?>
                 <tr>
-                    <th scope="col"><?php echo '約' . count($result) . '件' ?></th>
+                    <td><?php echo escape($value[0]); ?></td>
+                    <td><?php echo escape($value[1]); ?></td>
+                    <td><?php echo escape($value[2]); ?></td>
                 </tr>
-                <tr>
-                    <th>ID</th>
-                    <th>名前</th>
-                    <th>年齢</th>
-                </tr>
-                <?php foreach ($result as $key => $value) : ?>
-                    <tr>
-                        <td><?php echo escape($value[0]); ?></td>
-                        <td><?php echo escape($value[1]); ?></td>
-                        <td><?php echo escape($value[2]); ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            </table>
+            <?php endforeach; ?>
+        </table>
 
-    </main>
-    <footer class="footer p-3 bg-secondary bg-gradient text-white shadow"><small>HAL_PH_term2</small></footer>
+        </main>
+        <footer class="footer p-3 bg-secondary bg-gradient text-white shadow"><small>HAL_PH_term2</small></footer>
 
 
 </body>
